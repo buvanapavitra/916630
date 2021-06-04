6c4e4f52ee21d71a14cdba9df88fd00b168b0c47
buvanapavitra <buvanapavitra16@gmail.com>
2021-06-04 18:48:35 +0530

    this is my first commit

---
+++ React Handsons/eventexamplesapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Simple } from './Components/Simple';
+import { CurrencyConvertor } from './Components/CurrencyConvertor';
+
+function App() {
+  return (
+    <div >
+     <Simple/>
+     
+     <CurrencyConvertor/>
+    </div>
+  );
+}
+
+export default App;
